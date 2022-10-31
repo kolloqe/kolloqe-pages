c8b9e4cd682ca1f01188b48490c22fdade363393
Ishara Dissanayake <thisismaduishara@gmail.com>
2022-10-31 20:45:05 +0530

    added auto nav activation on scroll

--- res/scripts/kolloqe.js
+++ res/scripts/kolloqe.js
@@ -1,6 +1,31 @@
 $(document).ready(function () {
+  // autoset active nav on scroll
+  const sections = document.querySelectorAll(".mainsection");
+  const navLi = document.querySelectorAll("nav .navbar-collapse ul li");
+  console.log(sections);
+  window.addEventListener("scroll", () => {
+    let current = "kolloqe";
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (scrollY >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLi.forEach((li) => {
+      li.classList.remove("nav-link-active");
+      if (li.classList.contains(current)) {
+        li.classList.add("nav-link-active");
+      }
+    });
+  });
+
   // handling navigation
-  $(".nav-item").click(function (){
-    $(this).addClass("nav-link-active").siblings().removeClass("nav-link-active");
+  $(".nav-item").click(function () {
+    $(this)
+      .addClass("nav-link-active")
+      .siblings()
+      .removeClass("nav-link-active");
   });
 });
